1da66be2ca1ce0129fc150a9abc205ae27ece18c
abdoomr2003 <abdoomer1112003@gmail.com>
2024-05-30 00:16:10 +0300

    A

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,15 @@
 /* eslint-disable import/extensions */
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
+export default async function handleProfileSignup() {
   const upload = uploadPhoto();
   const create = createUser();
-  return Promise.all([upload, create])
-    .then((result) => {
-      console.log(
-        `${result[0].body} ${result[1].firstName} ${result[1].lastName}`
-      );
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+  try {
+        const result_2 = await Promise.all([upload, create]);
+        console.log(
+            `${result_2[0].body} ${result_2[1].firstName} ${result_2[1].lastName}`
+        );
+    } catch {
+        console.log('Signup system offline');
+    }
 }
